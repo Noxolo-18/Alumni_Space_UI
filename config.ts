4ddd4cf621619be15039eb10a60c2025d4f5b3b5
Noxolo <mnyalonoxolo@gmail.com>
2023-12-07 09:45:52 +0200

    updated

---
+++ config.ts
@@ -0,0 +1,11 @@
+// config.ts
+
+//server ip address
+ const host = '192.168.27.20';
+
+
+
+//export const baseUrl = 'http://192.168.27.20:3000/api';
+export const baseUrl = 'http://localhost:3000/api';
+export const chatUrl = 'http://192.168.27.16:3001';
+export const imageUrl = 'http://192.168.27.16:3000';
---
+++ src/app/components/image-slider/image-slider.component.ts
@@ -0,0 +1,33 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-slider',
+  templateUrl: './image-slider.component.html',
+  styleUrls: ['./image-slider.component.css']
+})
+export class ImageSliderComponent implements OnInit {
+
+  images: string[] = [
+    'assets/alumni.jpg',
+    'assets/alumni2.jpg',
+    'assets/alumni3.png',
+    'assets/alumni4.png',
+    'assets/alumni5.jpeg',
+    'assets/alumni6.jpeg'
+  ];
+
+  currentIndex = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.showSlides();
+  }
+
+  showSlides() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000); // Change slide every 3 seconds (adjust as needed)
+  }
+
+}
